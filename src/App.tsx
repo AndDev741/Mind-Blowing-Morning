bc3317e94295be1ef499abd3e17e2b41d58bf8e5
Vishal03-sv <patidarsham8@gmail.com>
2023-05-17 15:30:34 +0530

    Created rating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import Button from './components/Buttons/Button'
+import Rating from './components/Rating_Section/Rating'
 
 const App = () => {
   return (
     <>
     <Button outline="outline" >hello</Button>
+    <Rating/>
     </>
   )
 }
