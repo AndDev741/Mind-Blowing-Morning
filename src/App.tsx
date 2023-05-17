b37e9bfe477b6036a49124ce7fd9fdf06b618e10
sneharatnani <sneharatnani5@gmail.com>
2023-05-17 18:48:29 +0530

    complete mobile footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // import React from 'react'
-import Button from './components/Buttons/Button'
-import Footer from './components/footer/Footer'
+import Button from "./components/Buttons/Button";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
-  
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,13 @@ export default {
       colors: {
         lightGray: "#5C5C5C",
         primaryOrange: "#F47D31",
+        footerBg: "#FAF6F4",
+      },
+      boxShadow: {
+        footerInputShadow: "0px 3.33333px 3.33333px rgba(0, 0, 0, 0.25)",
+      },
+      borderRadius: {
+        footerBorderR: "6px 0px 0px 6px",
       },
     },
   },
