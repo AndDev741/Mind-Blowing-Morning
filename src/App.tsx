fc842837735309588f3f7208ed2536620ef31e53
Vishal03-sv <patidarsham8@gmail.com>
2023-05-28 16:21:36 +0530

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 // import React from 'react'
-<<<<<<< HEAD
-// import Button from './components/Buttons/Button'
-// import Rating from './components/Rating_Section/Rating'
-// import Footer from './components/footer/Footer'
-import About from './pages/About'
-=======
 import Button from "./components/Buttons/Button";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
->>>>>>> 99ca0c703a6bdfe5f7ac4dc060b692b589758c25
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-    {/* <Button outline="outline" >hello</Button> */}
-    {/* <Rating/> */}
-    {/* <Footer/> */}
-    <About/>
-=======
     <Home></Home>
     <Button style={{height:'300px'}}></Button>
->>>>>>> 99ca0c703a6bdfe5f7ac4dc060b692b589758c25
     </>
   );
 };
