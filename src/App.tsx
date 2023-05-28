c3b00450ff5c455e240b34985d9e01bab84e729e
Vishal03-sv <patidarsham8@gmail.com>
2023-05-28 15:50:29 +0530

    About section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 // import React from 'react'
-import Button from './components/Buttons/Button'
-<<<<<<< HEAD
-import Rating from './components/Rating_Section/Rating'
-=======
-import Footer from './components/footer/Footer'
->>>>>>> 90781e0d4ac1932311e8b5d14f5e2b56bb52e219
+// import Button from './components/Buttons/Button'
+// import Rating from './components/Rating_Section/Rating'
+// import Footer from './components/footer/Footer'
+import About from './pages/About'
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-    <Button outline="outline" >hello</Button>
-    <Rating/>
-=======
-  
->>>>>>> 90781e0d4ac1932311e8b5d14f5e2b56bb52e219
+    {/* <Button outline="outline" >hello</Button> */}
+    {/* <Rating/> */}
+    {/* <Footer/> */}
+    <About/>
     </>
   )
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+import './About.css'
+import './../index.css'
+import Button from '../components/Buttons/Button'
+
+export default function About() {
+  return (
+    <div className='section w-full flex justify-center items-center'>
+      <div className=" container  mt-3">
+        <div className="impara flex justify-row ">
+          <div className="bg">
+            <h4 className="heading">The simple <br/>solution for a <br/>productive <br/>
+              day is nothing <br/>but to <span>Start it <br/>early.</span><Button style={{padding:".4rem 1.4rem",margin:"7px"}}>Start</Button></h4>
+          </div>
+          <div className="paras ml-7">
+            <h4 className="heading1">We are aware, are you?</h4>
+            <p className="para">In today’s fast-paced world, the rat race is not normal. We don't even identify ourselves with the damage we do to ourselves.</p>
+            <p className="para">To address this cause and the importance of waking up early is our major agenda.</p>
+            <p className="para">In a way, it does feel like an accomplishment whenever you’re able to get up early. You can get started what you need to do, and hopefully be done early to enjoy the rest of your day.</p>
+            <p className="para">Waking up early is not just a win psychologically in that sense, it’s a win physically as well.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
