d4b3fbffa5f68369a1a8b04ec5465d93304b7034
unknown <ranamahato021@gmail.com>
2023-05-30 23:06:18 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 // import React from 'react'
-import Button from './components/Buttons/Button'
-<<<<<<< HEAD
-import Rating from './components/Rating_Section/Rating'
-=======
-import Footer from './components/footer/Footer'
->>>>>>> 90781e0d4ac1932311e8b5d14f5e2b56bb52e219
+// import Button from './components/Buttons/Button'
+// import Footer from './components/footer/Footer'
 
+import Hero from "./components/Hero/Hero"
+
+// import Rating from './components/Rating_Section/Rating'
+Hero
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-    <Button outline="outline" >hello</Button>
-    <Rating/>
-=======
-  
->>>>>>> 90781e0d4ac1932311e8b5d14f5e2b56bb52e219
+    <Hero/>
+     {/* <Button/>
+     <Rating/>
+     <Footer/> */}
+     
     </>
   )
 }
