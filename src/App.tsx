ebf4e694c2537a8efa13d7613f6fc06f024e56e1
sneharatnani <sneharatnani5@gmail.com>
2023-06-01 12:37:07 +0530

    start dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-// import React from 'react'
-// import { Route, Routes } from "react-router-dom";
-// import Button from "./components/Buttons/Button";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Button from "./components/Buttons/Button";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Courses from "./pages/Courses";
-// import Community from "./pages/Community";
-// import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import Community from "./pages/Community";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Dashboard />
       {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
