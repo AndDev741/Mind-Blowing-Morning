323ad288fff02c676d8235c3984d9911e1de8c72
Piyush batu <officialpiyushba2.com>
2023-06-02 16:53:47 +0530

    added course detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 // import React from 'react'
-import Button from './components/Buttons/Button'
+// import Button from './components/Buttons/Button'
+// import Success from './components/section_oursuccess/Success'
+// import Success from "./components/section_oursuccess/success"
+import Coursedetail from "./components/Course detail/Coursedetail"
+
 
 const App = () => {
   return (
     <>
-    <Button outline="outline" >hello</Button>
+    {/* <Button outline="outline" >hello</Button> */}
+    <Coursedetail/>
     </>
   )
 }
--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
+// import React, { useState } from 'react'
 import './Button.css'
 
 type buttonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   children? : string
   outline? : string
 }
-
+const btn = [ '1','2','3','4','5','6','7','8' ]
 const Button = ( props : buttonProps) => {
   // console.log( children , outline )
+
     return (
-    <button className={ `btn ${ props.outline }` } >
-        {
-           props.children 
-        }
-    </button>
+    btn.map( ( element )=> <button className='btn' id={ element } >{ element }</button>  )
   )
 }
 
